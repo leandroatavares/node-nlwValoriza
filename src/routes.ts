415bc1e9714fd256debf405f32b6c5982cb3b734
Original <leandro.tavares@original.com.br>
2021-06-24 21:07:51 -0300

    :sparkles:  - adiciona rota ao controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 
 const router = Router();
 
-const createUserConotroller = new CreateUserController()
+const createUserConotroller = new CreateUserController();
+const createTagController = new CreateTagController();
 
-router.post('/users', createUserConotroller.handle)
+router.post('/users', createUserConotroller.handle);
+router.post('/tags', createTagController.handle);
 
 export { router }
